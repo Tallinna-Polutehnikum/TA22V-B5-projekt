4eea2b391d4d7ac7a56b94ee08cd5d08130d76a1
Ervireb <Ervin.Rebane@tptlive.ee>
2024-09-26 16:14:34 +0300

    some_files
    Co-Authored-By: Nikita Andrejev <114184513+Chernobyc@users.noreply.github.com>

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import HomePage from './pages/HomePage/HomePage';
+// import MoviePage from './pages/MoviePage/MoviePage';
+// import CheckoutPage from './pages/CheckoutPage/CheckoutPage';
+
+import Header from './components/Header/header';
+import Footer from './components/Footer/footer';
+
+function App() {
+  return (
+    <div classname="wraper">
+        <Header />
+        
+      {/* на_потом
+      <Switch> 
+        <Route path="/" exact component={HomePage} />
+        <Route path="/movie/:id" component={MoviePage} />
+        <Route path="/checkout" component={CheckoutPage} />
+      </Switch> */}
+        <Footer />
+    </div>
+  )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
-require('dotenv').config();
-const express = require('express');
-const PORT = process.env.PORT;
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-const app = express();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  document.getElementById('root')
+);
 
 
-const start = async () => {
-    try{
-        //await sequelize.authenticate()
-        //await sequelize.sync()
-        app.listen(PORT, () => console.log(`Server was started on port ${PORT}`));        
-    } catch (e){
-        console.log(e)
-    }
-};
 
-start();
+// require('dotenv').config();
+// const express = require('express');
+// const PORT = process.env.PORT;
+// const app = express();
+
+// const start = async () => {
+//     try{
+//         //await sequelize.authenticate()
+//         //await sequelize.sync()
+//         app.listen(PORT, () => console.log(`Server was started on port ${PORT}`));        
+//     } catch (e){
+//         console.log(e)
+//     }
+// };
+
+// start();
