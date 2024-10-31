39f5244f92d281a643ec6a71bee93dbe91ac09d8
Chernobyc <114184513+Chernobyc@users.noreply.github.com.>
2024-10-31 19:45:42 +0200

    reinstall dep + add createRequire

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 require('dotenv').config();
 const express = require('express');
 const PORT = process.env.PORT;
