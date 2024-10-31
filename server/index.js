0bc7ba4cefd8328c427b33867dcf40dea50af590
Ervireb <Ervin.Rebane@tptlive.ee>
2024-10-31 18:11:10 +0200

    2 rows closer to run
    Co-Authored-By: Nikita Andrejev <114184513+Chernobyc@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -1,3 +1,6 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
 require('dotenv').config();
 const express = require('express');
 const PORT = process.env.PORT;
