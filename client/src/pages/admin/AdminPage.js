ee99c4934565d481ab5debf5cfb2e8c527647a4e
Ervireb <Ervin.Rebane@tptlive.ee>
2025-03-10 15:27:19 +0200

    redo_admin
    next: copy in each page or make as component

--- client/src/pages/admin/AdminPage.js
+++ client/src/pages/admin/AdminPage.js
@@ -1,104 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import HomePage from '../HomePage';
+import MovieAdd from './MovieAdd';
+import MovieEdit from './MovieEdit';
 import logo from '../../logo.svg';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './admin.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClipboardUser, faClapperboard, faChevronUp,
-  faPen, faPlus, faMinus, faFire, faStar, faCircleUser
-  } from '@fortawesome/free-solid-svg-icons';
+import { faClipboardUser, faChevronUp, faChevronDown, faCircleUser, faClapperboard, faPen, faPlus, faMinus, faFire } from '@fortawesome/free-solid-svg-icons';
 import { faGoogle as faGoogleBrand } from '@fortawesome/free-brands-svg-icons';
 
+// ================================================================================================================
+
 const AdminPage = () => {
+  const [profileOpen, setProfileOpen] = useState(true);
+  const [moviesOpen, setMoviesOpen] = useState(true);
+
   return (
-    <div>
+    <div className="app-container">
+      <div id="sidebar" className="sidebar">
+        <ul>
+          <li><a onClick={() => setProfileOpen(!profileOpen)} className="nav-link"> <FontAwesomeIcon icon={faClipboardUser} />
+              Profile {profileOpen ? <FontAwesomeIcon icon={faChevronUp} className="float-right" /> : <FontAwesomeIcon icon={faChevronDown} className="float-right" />}
+            </a>
+            <ul className={profileOpen ? "show" : "collapse"}>
+              <li><Link to="/profile" className="nav-link">
+                <FontAwesomeIcon icon={faCircleUser} />
+                Full Name
+              </Link></li>
+              <li><Link to="/editprofile" className="nav-link">
+                <FontAwesomeIcon icon={faCircleUser} />
+                Edit Profile
+              </Link></li>
+            </ul>
+          </li>
 
-      
-    <div className="sidebar">
-      <ul>
-        <li className="no-bullets">
-          <a href="#submenu1" data-bs-toggle="collapse" className="nav-link link-light align-middle">
-            <FontAwesomeIcon icon={faClipboardUser} />
-            Profile <FontAwesomeIcon icon={faChevronUp} className="float-right" />
-          </a>
-          <ul className="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-
-            <li><a href=" " target="iframe_a">
-                <img className="rounded-circle" style={{ width: 20, height: 20 }} src="https://mdbootstrap.com/img/Photos/Avatars/img%20(21).jpg" /> Full name
-              </a>
-            </li>
-
-            <li><a href="./pages/Edit_profile.html" target="iframe_a">
-                <FontAwesomeIcon icon={faCircleUser} /> Edit profile <sup>(...)</sup>
-              </a>
-            </li>
-            
-          </ul>
-        </li>
-        <li className="no-bullets">
-          <a href="#submenu2" data-bs-toggle="collapse" className="nav-link link-light align-middle">
-            <FontAwesomeIcon icon={faClapperboard} />
-            Movies <FontAwesomeIcon icon={faChevronUp} className="float-right" />
-          </a>
-          <ul className="collapse show nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
-
-            <li><a href="/admin/MovieEdit" target="iframe_a">
-                <FontAwesomeIcon icon={faPen} /> Edit Movie <sup>(...)</sup>
-              </a>
-            </li>
-
-            <li><a href="/admin/movieAdd" target="iframe_a"> 
-                <FontAwesomeIcon icon={faPlus} /> Add Movie <sup>(working)</sup>
-              </a>
-            </li>
-
-            <li><a href="#" target="iframe_a"> 
-                <FontAwesomeIcon icon={faMinus} /> Remove Movie
-              </a>
-            </li>
-
-            <li><a href="#" target="iframe_a"> 
-                <FontAwesomeIcon icon={faFire} /> Popular Movies
-              </a>
-            </li>
-
-            <li><a href="/admin/movieAdd"> 
-                <FontAwesomeIcon icon={faStar} /> Favourite Movies
-              </a>
-            </li>
-
-          </ul>
-        </li>
-        <a href="#hotkeys"></a>
-        <a href="#google">
-          <FontAwesomeIcon icon={faGoogleBrand} />Google Analytics
-        </a>
-
-        <footer style={{ position: 'fixed', bottom: 0, height: 60, width: 'inherit', display: 'block' }} className="align-items-center">
-          <h5 className="link-light">
-            <img
-                    src={logo}
-                    width="50" height="50"
-                    className="ms-4"
-                    alt="logo"
-                  />
-                  {' Apollo prj'}
-          </h5>
-        </footer>
-      </ul>
-    </div>
-
-      <div className="main-absolute">
-        <iframe
-          title="Admin Content"
-          name="iframe_a"
-          className="rounded-3"
-          style={{ height: '100%', width: '100%' }}
-          src="/about"
-        />
+          <li><a onClick={() => setMoviesOpen(!moviesOpen)} className="nav-link"> <FontAwesomeIcon icon={faClapperboard} />
+              Movies {moviesOpen ? <FontAwesomeIcon icon={faChevronUp} className="float-right" /> : <FontAwesomeIcon icon={faChevronDown} className="float-right" />}
+            </a>
+            <ul className={moviesOpen ? "show" : "collapse"}>
+              <li><Link to="/admin/movieEdit" className="nav-link">
+                <FontAwesomeIcon icon={faPen} />
+                Edit Movie
+              </Link></li>
+              <li><Link to="/admin/movieAdd" className="nav-link">
+                <FontAwesomeIcon icon={faPlus} />
+                Add Movie
+              </Link></li>
+              <li><Link to="/admin/movieRemove" className="nav-link">
+                <FontAwesomeIcon icon={faMinus} />
+                Remove Movie
+              </Link></li>
+              <li><Link to="/admin/popularMovies" className="nav-link">
+                <FontAwesomeIcon icon={faFire} />
+                Popular Movies
+              </Link></li>
+            </ul>
+          </li>
+          
+          <li><Link href="#google" className="nav-link">
+              <FontAwesomeIcon icon={faGoogleBrand} />
+              Google Analytics
+            </Link>
+          </li>
+          <footer id="logofooter">
+            <h5 className="link-light">
+              <img
+                src={logo}
+                width="50" height="50"
+                className="ms-4"
+                alt="logo"
+              />
+              {' Apollo prj'}
+            </h5>
+          </footer>
+        </ul>
+      </div>
+      <div className="content">
+        <Routes>
+          <Route path="/profile" element={<div>Profile Page</div>} />
+          <Route path="/editprofile" element={<div>Edit Profile Page</div>} />
+          <Route path="/movieedit" element={<div>Movie Edit Page</div>} />
+          <Route path="/movieadd" element={<div>Movie Add Page</div>} />
+          <Route path="/removemovie" element={<div>Remove Movie Page</div>} />
+          <Route path="/popularmovies" element={<div>Popular Movies Page</div>} />
+          <Route path="/favouritemovies" element={<div>Favourite Movies Page</div>} />
+        </Routes>
       </div>
     </div>
   );
 };
 
 export default AdminPage;
+
